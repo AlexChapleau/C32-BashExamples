06921c2b135944173061ff0f6f99425190f73045
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 13:56:58 -0400

    q6 fait

---
+++ Lab-Q6.sh
@@ -0,0 +1,41 @@
+# Afficher des options à l’usager et exécuter le choix parmi les suivants :
+# 	Si on tape la lettre q ou Q, quitter le programme (en sortant de la boucle)
+#	Si on tape la lettre a  ou A, afficher le contenu du répertoire /root
+#	Si on tape la lettre b ou B, afficher le contenu du fichier /etc/passwd.
+# Avant de réafficher le menu, effacer l'écran. Utiliser une boucle until.
+
+
+afficherMenu(){
+    clear
+    echo "------------------------"
+    echo "-   Labo bash Q6 101   -"
+    echo "------------------------"
+    echo "- a ou A = afficher le contenu du répertoire /root"
+    echo "- b ou B = afficher le contenu du fichier /etc/passwd"
+    echo "- q ou Q = quitter"
+    
+}
+
+fin=0
+until [[ $fin = 1 ]]
+do
+    afficherMenu
+    read -p "Entrez un choix: " choix
+    case $choix in 
+        [a,A])
+            ls ../../
+            sleep 2
+            ;; 
+        [b,B])
+            cat ../../../etc/passwd
+            sleep 2
+            ;; 
+        [q,Q])
+            fin=1
+            ;; 
+        *) 
+            echo "invalide"
+            sleep 2
+        ;;
+    esac
+done
