508a94491036776e06226e6632d6955cba1bb3dc
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 14:29:53 -0400

    q9 presque fini

---
+++ Lab-Q9.sh
@@ -0,0 +1,46 @@
+# Créez la commande « doom », qui pourra être exécutée à partir de n’importe où (placez-la donc dans le bon dossier). Celle-ci, étant destructrice, fera semblant de détruire les fichiers du répertoire actuel, un peu similaire à celui d’un virus infectant une machine. Voici le résultat ma version de « doom »
+
+
+
+clear
+echo "==========================="
+echo "  !! YOU WERE INFECTED !!  "
+echo "==========================="
+echo "---------------------------"
+echo " Say goodbye to your files "
+echo "---------------------------"
+
+liste=`ls`
+
+for fichier in $liste
+do  
+    echo "$fichier ... Deleted"
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
