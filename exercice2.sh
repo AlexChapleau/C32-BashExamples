b464ab992e84c70bda4bd359ce510c0faa018cd5
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 12:23:38 -0400

    exo2 et 3 fait

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+#1 - afficher menu
+#2 - mettre le menu dans une fonction
+#3 - faire une boucle
+#       -offrir 2 choix: quitter(choix q)
+#       -appelez ./exercice1.sh (choix d)
+#4 - a chaque debut de boucle, afficher le menu
+
+
+afficherMenu(){
+    clear
+    echo "------------------------"
+    echo "- Logiciel 101         -"
+    echo "------------------------"
+    echo "- d = dictionnaire"
+    echo "- q = quitter"
+    
+}
+
+choix=
+while [[ $choix != q ]]
+do
+    afficherMenu
+    read -p "Entrez un choix: " choix
+    if [[ $choix = "d" ]]
+    then    
+        ./exercice1.sh 
+        sleep 1
+    fi
+done
--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+
+faireEspace(){
+
+for ((i=0;i<$1;i++))
+    do
+        echo -n " "
+    done
+}
+
+n=1
+for ((i=0;i<20;i++))
+do
+    clear
+    faireEspace $i
+    if [[ $i -lt 19 ]]
+    then
+        echo "@"
+    else
+        echo "BOOM"
+    fi
+    sleep 0.1
+done
+
+
+
+
