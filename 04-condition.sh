adf4530e382255e8b6e2ad0af486e3b835b9055e
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 10:23:35 -0400

    ex4 fin

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+note=61
+
+#if test ...
+#if [[...]]
+
+#if [[ $note -lt 60 ]];then  
+
+if [[ $note -lt 60 ]]  
+then
+    echo "Echec"
+elif test $note -eq 60
+then
+    echo "safe"
+else
+    echo "bravo"
+fi
+
+read -p "Entrez une lettre " lettre
+
+case $lettre in 
+    [[:lower]])
+        echo "la lettre est en minuscule"
+        ;; #break
+    *) #autre,default
+        echo "Autre"
+    ;;
+esac
 
 
-
-
-
-
+if [[ $lettre != "a" ]]
+then    
+    echo "diff de a"
+fi
 
