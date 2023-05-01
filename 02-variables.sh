aa20a1233caf4cf3659b83f9fb88799735d6f739
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 09:57:20 -0400

    ex 2 complet

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#pas despace entre le =, car sinon ca ne marche pas
+prenom=Neo
+
+echo $prenom
+
+#declare un int 
+declare -i age=33
+
+#declare une variable constante quon ne peut modifier
+declare -r CHEMIN=/bin
+
+echo "Nom script" $0
+echo "Nombre params" $# 
+echo "Perso" $1
+
+
+
