b323063f78c3225df2949bff7cf6039d542eb21b
Alexandre Chapleau <alexandrechapleau@hotmail.com>
2023-05-01 09:45:50 -0400

    ex 1 complet

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "hello" $USER
+
+# exit = permet de quitter le programme, tout le code apres ne va pas etre lu
+
+echo -n "Vivre "
+echo "Linux"
+
+# -e permet d'interpreter les retours de ligne et etc (\n,\t,...)
+echo -e "1\n2\n3"
+
+read -p "Quel age avez-vous? " age
+echo $age "! vous etes jeune"
+
+
